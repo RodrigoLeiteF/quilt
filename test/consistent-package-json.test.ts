35bded96462ccb4a3b6e6581815b962ef10cf19e
Sam Bostock <sam.bostock@shopify.com>
2020-01-11 23:49:07 -0500

    Ensure package.json fields match template

--- test/consistent-package-json.test.ts
+++ test/consistent-package-json.test.ts
@@ -3,19 +3,119 @@ import {dirname, join, relative, resolve} from 'path';
 import {readJSONSync} from 'fs-extra';
 import glob from 'glob';
 
+const KNOWN_TEMPLATE_KEYS = [
+  'author',
+  'bugs',
+  'dependencies',
+  'description',
+  'files',
+  'homepage',
+  'license',
+  'main',
+  'name',
+  'publishConfig',
+  'repository',
+  'scripts',
+  'sideEffects',
+  'types',
+  'version',
+];
+
 const ROOT_PATH = resolve(__dirname, '..');
 const packageJSONTemplatePath = join('templates', 'package.hbs.json');
 const packageJSONTemplate = readJSONSync(
   join(ROOT_PATH, packageJSONTemplatePath),
 );
 
+// eslint-disable-next-line jest/valid-describe
+describe(packageJSONTemplatePath, () => {
+  it('has only known keys', () => {
+    const keys = Object.keys(packageJSONTemplate).sort();
+
+    // Template keys should match exactly. If updating it, remember to update tests!
+    expect(keys).toStrictEqual(KNOWN_TEMPLATE_KEYS);
+  });
+});
+
 readPackages().forEach(({packageName, packageJSON, packageJSONPath}) => {
   // eslint-disable-next-line jest/valid-describe
   describe(packageJSONPath, () => {
+    it('specifies Shopify as author', () => {
+      expect(packageJSON.author).toBe(packageJSONTemplate.author);
+    });
+
+    it('specifies Quilt Issues as bugs URL', () => {
+      expect(packageJSON.bugs).toStrictEqual(packageJSONTemplate.bugs);
+    });
+
+    it('specifies a dependencies', () => {
+      expect(packageJSON.dependencies).not.toStrictEqual({});
+    });
+
+    it('specifies a description', () => {
+      expect(packageJSON.description).not.toBeUndefined();
+    });
+
+    it('specifies files', () => {
+      if(packageName == 'polyfills') return; // FIXME
+
+      expect(packageJSON.files).toContain('dist/*');
+      expect(packageJSON.files).toContain('!tsconfig.tsbuildinfo');
+    });
+
+    it('specifies Quilt deep-link homepage', () => {
+      const expectedHomepage = compile(packageJSONTemplate.homepage);
+
+      expect(packageJSON.homepage).toBe(expectedHomepage);
+    });
+
+    it('specifies the MIT license', () => {
+      expect(packageJSON.license).toBe(packageJSONTemplate.license);
+    });
+
+    it('specifies the expected main', () => {
+      if(packageName === 'graphql-persisted') return; // FIXME: Address this in graphql-persisted
+
+      expect(packageJSON.main).toBe(packageJSONTemplate.main);
+    });
+
     it('specifies name matching scope and path', () => {
       const expectedName = compile(packageJSONTemplate.name);
 
       expect(packageJSON.name).toBe(expectedName);
+    });
+
+    it('specifies the expected publishConfig', () => {
+      expect(packageJSON.publishConfig).toStrictEqual(
+        packageJSONTemplate.publishConfig,
+      );
+    });
+
+    it('specifies a repository deep-linking into the Quilt monorepo', () => {
+      const expectedRepository = compile(packageJSONTemplate.repository);
+
+      expect(packageJSON.repository).toBe(expectedRepository);
+    });
+
+    it('specifies scripts, including build', () => {
+      expect(packageJSON.scripts.build).toBe(packageJSONTemplate.scripts.build);
+    });
+
+    it('specifies if it has sideEffects', () => {
+      const exceptions = ['csrf-token-fetcher', 'dates', 'react-effect', 'react-google-analytics', 'useful-types']
+      if (exceptions.includes(packageName)) return; // FIXME
+
+      expect(packageJSON.sideEffects).toBe(Boolean(packageJSON.sideEffects));
+    });
+
+    it('specifies the expected types', () => {
+      if(packageName === 'graphql-persisted') return; // FIXME
+
+      expect(packageJSON.types).toBe(packageJSONTemplate.types);
+    });
+
+    it('specifies a version', () => {
+      expect(packageJSON.version).not.toBeUndefined();
     });
   });
 
